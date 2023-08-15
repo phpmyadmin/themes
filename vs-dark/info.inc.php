3f08f3dc4120bb7b86580ac0b9723cbed9fe2c6f
MrMiumo <miumo.contact@gmail.com>
2023-08-15 20:19:23 +0200

    VsDark v1.0

---
+++ vs-dark/info.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is information for VsDark theme
+ * If you have problems or questions, email miumo.contact@gmail.com
+ *
+ * @package VsDark
+ * @author  Miumo
+ * @link    https://github.com/MrMiumo
+ */
+
+// Theme info
+$theme_name = 'VsDark';
+$theme_full_version = '1.0';
